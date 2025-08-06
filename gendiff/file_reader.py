915c797c111cdc8b77b72561e9f0cb716fd7a822
Яна Трясцына <yana.tryastsyna@mail.rua1$ git config --global user.email yana.tryastsyna@mail.ru>
2025-08-07 01:16:53 +0500

    add diff for yaml files

--- gendiff/file_reader.py
+++ gendiff/file_reader.py
@@ -1,8 +1,16 @@
+import os
 import json
+import yaml
 
 def read_file(path):
+    _, ext = os.path.splitext(path)
     with open(path) as f:
-        return json.load(f)
+        if ext == '.json':
+            return json.load(f)
+        elif ext in ('.yml', '.yaml'):
+            return yaml.safe_load(f)
+        else:
+            raise ValueError(f'Unsupported file format: {ext}')
+    
     
 
-   
---
+++ tests/test_flat_diff_yaml.py
@@ -0,0 +1,18 @@
+from gendiff import generate_diff
+
+
+def test_flat_diff_yaml():
+    file1 = 'tests/test_data/file1.yaml'
+    file2 = 'tests/test_data/file2.yaml'
+
+    expected = '''{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}'''
+
+    result = generate_diff(file1, file2)
+    assert result.strip() == expected.strip()
