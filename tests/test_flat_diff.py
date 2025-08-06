6696bd1f12521eba3c5b52988dbf4e431117c980
Яна Трясцына <yana.tryastsyna@mail.rua1$ git config --global user.email yana.tryastsyna@mail.ru>
2025-08-06 16:01:51 +0500

    update pyptoject.toml

--- tests/test_flat_diff.py
+++ tests/test_flat_diff.py
@@ -1,22 +1,18 @@
-import os
 from gendiff import generate_diff
 
 
-
 def test_flat_diff():
-    current_dir = os.path.dirname(__file__)
-
-    file1 = os.path.join(current_dir, 'test_data', 'file1.json')
-    file2 = os.path.join(current_dir, 'test_data', 'file2.json')
+    file1 = 'tests/test_data/file1.json'
+    file2 = 'tests/test_data/file2.json'
 
     expected = '''{
-  - follow: False
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: True
-}'''
+      - follow: False
+        host: hexlet.io
+      - proxy: 123.234.53.22
+      - timeout: 50
+      + timeout: 20
+      + verbose: True
+    }'''
 
     result = generate_diff(file1, file2)
     assert result.strip() == expected.strip()
