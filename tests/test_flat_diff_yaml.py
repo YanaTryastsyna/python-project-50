f56a8a11fc9cc564b77f1ab89b72e92f47c397c4
Яна Трясцына <yana.tryastsyna@mail.rua1$ git config --global user.email yana.tryastsyna@mail.ru>
2025-08-10 00:35:10 +0500

    upgrade tetst

--- tests/test_flat_diff_yaml.py
+++ tests/test_flat_diff_yaml.py
@@ -4,15 +4,11 @@ def test_flat_diff_yaml():
     file1 = 'tests/test_data/file1.yml'
     file2 = 'tests/test_data/file2.yml'
 
-    expected = '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+    with open('tests/test_data/expected_diff.txt') as f:
+        expected = f.read()
 
     result = generate_diff(file1, file2)
     assert result.strip() == expected.strip()
 
+
+  
